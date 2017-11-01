82df2a21d2b1d9287c5013e2ba0f6c69dce93670
dreiv <dreivoicu@gmail.com>
2017-11-01 18:38:52 +0200

    testing build.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<h1>Hello World!</h1>`,
+    template: `<h1>hello world!</h1>`,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
