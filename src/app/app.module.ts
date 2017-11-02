73669aa69fb84c57f95cd7a4c65e7b3d20de8579
dreiv <dreivoicu@gmail.com>
2017-11-02 22:29:04 +0200

    chore: updated dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        ServiceWorkerModule.register('/ngsw-worker.js')
     ],
     providers: [],
     bootstrap: [AppComponent]
